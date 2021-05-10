103813d2be75e91543e7e444bef1c1262c806761
Nipun Kohli <nipunk75@gmail.com>
2021-05-10 16:36:33 +0530

    changed redirect uri

--- src/components/config.js
+++ src/components/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "10bfd12792394f2db9804268f92ea658";
-export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "https://spotifypro.vercel.app/redirect";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
